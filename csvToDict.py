e72a2b183cb2517c2127b211da231df5d58313c5
Helen Ye <00yhelen@gmail.com>
2016-06-06 13:17:18 -0400

    Deal with having username already existing and combine login and register to one page

--- csvToDict.py
+++ csvToDict.py
@@ -27,3 +27,15 @@ def csvToDict( fileName):
     return dOL
 
 # print csvToDict( 'babe.csv')
+
+def userToDict(_file):
+    src = open(_file, 'rU')
+    csvLines = []
+    d = {}
+    for line in src:
+        csvLines.append(line.strip('\n').split(','))
+    for i in range(len(csvLines)):
+        d[csvLines[i][0]] = csvLines[i][1]
+    return d
+
+# print userToDict('accounts.csv')
--- regis.py
+++ regis.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 
-import cgitb,cgi,html
+import cgitb,cgi,html,csvToDict
 cgitb.enable()
 
 print 'content-type: text/html\n'
 
 qs = cgi.FieldStorage()
 
-dest = open('accounts.csv', 'a', 0)
+curr = csvToDict('accounts.csv')
+
 user = qs['user'].value
 pw = qs['pw'].value
 re = qs['retype'].value
+
 if pw == re:
+    dest = open('accounts.csv', 'a', 0)
     dest.write('\n' + user + ',' + pw)
     print html.h(1, 'SUCCESS! Now login on the main page.') #REDIRECT TO LOGIN PAGE
-else:
+else if pw != re:
     print html.h(1, 'The passwords do not match!') #IF POSSIBLE, REDIRECT TO REGISTER PAGE
+else if user in curr:
+    print html.h(1, 'This username already exists.') #REDIRECTIONNNNNNN
