10cfd8fca0d26bd70fd1f04b50a637293dc9deab
Helen Ye <00yhelen@gmail.com>
2016-06-07 15:02:41 -0400

    Debugging woo

--- login.py
+++ login.py
@@ -7,27 +7,29 @@ print 'content-type: text/html\n'
 
 qs = cgi.FieldStorage()
 
-curr = csvToDict('accounts.csv')
-
-if reguser in qs:
+curr = csvToDict.userToDict('accounts.csv')
+print curr
+if 'reguser' in qs:
     reguser = qs['reguser'].value
     regpw = qs['regpw'].value
     re = qs['retype'].value
-    if pw == re:
+    if regpw == re and not reguser in curr:
         dest = open('accounts.csv', 'a', 0)
-        dest.write('\n' + user + ',' + pw)
-        print html.h(1, 'SUCCESS! Now login on the main page.') #REDIRECT TO LOGIN PAGE
-    else if pw != re:
-        print html.h(1, 'The passwords do not match!') #IF POSSIBLE, REDIRECT TO REGISTER PAGE
-    else if user in curr:
-        print html.h(1, 'This username already exists.') #REDIRECTIONNNNNNN\
+        dest.write('\n' + reguser + ',' + regpw)
+        dest.close()
+        print html.heading(1, 'SUCCESS! Now login on the main page.') #REDIRECT TO LOGIN PAGE
+    elif reguser in curr:
+        print html.heading(1, 'This username already exists.') #REDIRECTIONNNNNNN\
+    elif regpw != re:
+        print html.heading(1, 'The passwords do not match!') #IF POSSIBLE, REDIRECT TO REGISTER PAGE
 else:
     user = qs['user'].value
     pw = qs['pw'].value
-    src = open('accounts.csv', 'rU')
-    if user in curr and curr[user] == pw:
-        #login success stuff
+    if user in curr:
+        if curr[user] == pw:
+            print html.heading(1, 'success')#login success stuff
+        else:
+            print html.heading(1, 'Incorrect password. Please try again.') #Redirect to homepage
     else:
-        print html.h(1, 'Incorrect username or password. Please try again.') #Redirect to homepage
-
-#i am a super messy programmer whoops
+        print html.heading(1, 'Incorrect username. Please try again.') #Redirect to homepage
+#i am a super messy programmer whoops'''
